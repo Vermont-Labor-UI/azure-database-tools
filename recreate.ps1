ccc848c80716d019d323cd0c251623388ae30000
Chris Woerz <erendrake@gmail.com>
2018-05-09 23:00:44 -0600

    removed function to make it work

--- recreate.ps1
+++ recreate.ps1
@@ -1,24 +1,22 @@
-function Clear-Database {
-    param(
-        [Parameter(Mandatory=$true)][string]$resourceGroup,
-        [Parameter(Mandatory=$true)][string]$databaseServer,
-        [Parameter(Mandatory=$true)][string]$databaseName
-    )
+param(
+    [Parameter(Mandatory=$true)][string]$resourceGroup,
+    [Parameter(Mandatory=$true)][string]$databaseServer,
+    [Parameter(Mandatory=$true)][string]$databaseName
+)
 
-    Write-Host "Starting to recrate database: $databaseName on server $databaseServer"
-    $database = Get-AzureRmSqlDatabase  -ResourceGroupName $resourceGroup `
-        -ServerName $databaseServer `
-        -DatabaseName $databaseName `
+Write-Host "Starting to recrate database: $databaseName on server $databaseServer"
+$database = Get-AzureRmSqlDatabase  -ResourceGroupName $resourceGroup `
+    -ServerName $databaseServer `
+    -DatabaseName $databaseName `
 
-    if (-Not $database.ElasticPoolName) {
-        Write-Host "Cannot detect ElasticPool failing build"
-    }
-    Remove-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
-        -ServerName $databaseServer `
-        -DatabaseName $databaseName
-
-    New-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
-        -ServerName $databaseServer `
-        -DatabaseName $databaseName `
-        -ElasticPoolName $database.ElasticPoolName
+if (-Not $database.ElasticPoolName) {
+    Write-Host "Cannot detect ElasticPool failing build"
 }
+Remove-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
+    -ServerName $databaseServer `
+    -DatabaseName $databaseName
+
+New-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
+    -ServerName $databaseServer `
+    -DatabaseName $databaseName `
+    -ElasticPoolName $database.ElasticPoolName
