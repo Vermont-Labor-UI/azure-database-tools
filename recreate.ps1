847d866acc6227545163e2619d25794a73135e0a
Chris Woerz <chriswoerz@gmail.com>
2018-10-05 11:49:16 -0600

    more logging, formatting

--- recreate.ps1
+++ recreate.ps1
@@ -1,13 +1,13 @@
 param(
-    [Parameter(Mandatory=$true)][string]$resourceGroup,
-    [Parameter(Mandatory=$true)][string]$databaseServer,
-    [Parameter(Mandatory=$true)][string]$databaseName,
-    [Parameter(Mandatory=$true)][string]$dacpacFilePath,
-    [Parameter(Mandatory=$false)][Boolean]$skipRecreate
+    [Parameter(Mandatory = $true)][string]$resourceGroup,
+    [Parameter(Mandatory = $true)][string]$databaseServer,
+    [Parameter(Mandatory = $true)][string]$databaseName,
+    [Parameter(Mandatory = $true)][string]$dacpacFilePath,
+    [Parameter(Mandatory = $false)][Boolean]$skipRecreate
 )
 
 Write-Host "Looking for file $dacpacFilePath"
-if ([System.IO.File]::Exists($dacpacFilePath) -eq $false){
+if ([System.IO.File]::Exists($dacpacFilePath) -eq $false) {
     Write-Host "File $dacpacFilePath not found, aborting"
     exit 1
 }
@@ -22,20 +22,28 @@ if (-Not $database.ElasticPoolName) {
     exit 1
 }
 
-if ($PSBoundParameters.ContainsKey('skipRecreate')){
-    if($skipRecreate){
+if ($PSBoundParameters.ContainsKey('skipRecreate')) {
+    if ($skipRecreate) {
         Write-Host "Skipping recreate due to presence of flag"
         exit 0
     }
 }
 
 Write-Host "Removing database: $databaseName on server $databaseServer"
-Remove-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
+$removeResult = Remove-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
     -ServerName $databaseServer `
     -DatabaseName $databaseName
+Write-Host ($removeResult | Format-List | Out-String)
 
 Write-Host "Creating database: $databaseName on server $databaseServer"
-New-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
-    -ServerName $databaseServer `
-    -DatabaseName $databaseName `
-    -ElasticPoolName $database.ElasticPoolName
+try {
+    $newResult = New-AzureRmSqlDatabase -ResourceGroupName $resourceGroup `
+        -ServerName $databaseServer `
+        -DatabaseName $databaseName `
+        -ElasticPoolName $database.ElasticPoolName
+    Write-Host ($newResult | Format-List | Out-String)
+}
+catch {
+    Write-Host 'We Caught  '  + $_.Exception.GetType().FullName -fore blue -back white
+    Write-Host ($_.Exception | Format-List | Out-String)
+}
